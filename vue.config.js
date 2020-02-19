ef7539bd20f1f05965eaf85a74ab0c436da9eafa
yangyuezheng <yangyuezheng@g7.com.cn>
2020-02-19 23:18:10 +0800

    feat: 调整页面title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// const isProduction = process.env.NODE_ENV === 'production'
-// module.exports = {
-//     publicPath: isProduction ? /xgplayer : './',
-// }
+const isProduction = process.env.NODE_ENV === 'production'
+module.exports = {
+    publicPath: isProduction ? '/xgplayer-demo' : './',
+}
