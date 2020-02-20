db7b8c6e307128c78c172b8ac3c546d654c52d9e
yangyuezheng <yangyuezheng@g7.com.cn>
2020-02-20 10:43:18 +0800

    feat: 调整播放器语言为中文

--- src/assets/playerConfig.js
+++ src/assets/playerConfig.js
@@ -7,6 +7,7 @@ export default {
   playbackRate: [0.5, 0.75, 1, 1.5, 2],
   defaultPlaybackRate: 1,
   playsinline: true,
+  lang: 'zh-cn',
   poster:
     "https://s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg",
   url: "https://s1.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4"
